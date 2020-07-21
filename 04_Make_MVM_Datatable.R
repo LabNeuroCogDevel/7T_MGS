912b0c5e48935a52f77f0f9f15c20cc286b3d2df
WillForan <willforan@gmail.com>
2020-07-21 16:11:17 -0400

    🎨 simpilify MVM datatable builder

--- 04_Make_MVM_Datatable.R
+++ 04_Make_MVM_Datatable.R
@@ -1,83 +1,54 @@
+#!/usr/bin/env Rscript
 #
 #Author: Maria Perica
 #Title: 04_Make_MVM_Datatable.R
 #Purpose: To create data table for use in 3dMVM script
 #
 #
-library(readxl)
 library(tidyr)
 library(dplyr)
-library(reshape2)
-library(ggplot2)
-library(purrr)
 library(LNCDR)
 library(lubridate)
 
 
-#make table of subjects
-#subjs <- read.table('/Volumes/Zeus/Orma/7T_MGS/data/subjects.txt',header=T)
+# output of ./02.1_decon_subj_list.bash
+decon_subjs <- read.table('/Volumes/Zeus/Orma/7T_MGS/data/have_decon_2020-07-21.txt', header=T) %>%
+    rename(ld8=Subj) %>%
+  separate("ld8", into=c("Subj","vdate"), remove=FALSE) %>%
+    mutate(vdate=ymd(vdate))
 
-subjs <- read.table('/Volumes/Zeus/Orma/7T_MGS/data/have_decon_2020-07-21.txt',header=T)
+# use database to get dob so we can calculate age. will merge on lunaid part of d8
+database <- LNCDR::db_query("select id,sex,dob from person natural join enroll where etype like 'LunaID'") %>%
+    mutate(dob=ymd(dob))
 
-subjs <- separate(subjs, c("Subj"), into=c("Subj","vdate"),remove=FALSE)
+# use luna_date to find files for each visit
+# exclude subject if they don't exist. also get age
+filepatt = '/Volumes/Zeus/Orma/7T_MGS/data/%s/cenfd0.5/%s_bucket+tlrc.HEAD'
+subjs <- merge(decon_subjs, database, by.x="Subj", by.y="id", all.x=T, all.y=F) %>%
+  mutate(age=as.numeric(vdate-dob)/365.25) %>%
+  mutate(headfile=sprintf(filepatt,ld8,ld8)) %>%
+  filter(file.exists(headfile))
 
+if(nrow(subjs)<=0L) stop('no files like ',filepatt)
+if(nrow(subjs) != nrow(decon_subjs))
+    warning('lost some subjects in db merge or file check! ', setdiff(decon_subjs$Subj, subjs$Subj) %>% paste0(collapse=", "))
 
-database <- LNCDR::db_query("select id,sex,dob from person natural join enroll where etype like 'LunaID'")
-
-database$dob <- ymd(database$dob)
-
-subjs$vdate <- ymd(subjs$vdate)
-
-subjs <-
-  
-  merge(subjs,database,by.x=c("Subj"), by.y=c("id"),all.x=T,all.y=F) %>%
-  
-  mutate(age=as.numeric(vdate-dob)/365.25)
-
-#repeat each dataframe 3 times
-cue <- subjs
-delay <- subjs
-resp <- subjs
-
-#create a condition column
-cue$condition <- "cue"
-delay$condition <- "delay"
-resp$condition <- "resp"
-
-#make list of subject IDs
-replist <- as.list(subjs$Subj)
-#create input file column to point to bucket file for each subject
-InputFile <-sapply(replist, function(x) sprintf('/Volumes/Zeus/Orma/7T_MGS/data/%s/cenfd0.5/%s_bucket+tlrc.HEAD', x,x))
-inputfileexist <-sapply(InputFile, file.exists)
-InputFile_cue <- lapply(replist, function(x) sprintf('/Volumes/Zeus/Orma/7T_MGS/data/%s/cenfd0.5/%s_bucket+tlrc.HEAD[cue#0_Coef]', x,x))
-InputFile_delay<- lapply(replist, function(x) sprintf('/Volumes/Zeus/Orma/7T_MGS/data/%s/cenfd0.5/%s_bucket+tlrc.HEAD[delay#0_Coef]', x,x))
-InputFile_resp <- lapply(replist, function(x) sprintf('/Volumes/Zeus/Orma/7T_MGS/data/%s/cenfd0.5/%s_bucket+tlrc.HEAD[resp#0_Coef]', x,x))
-
-InputFile <- unlist(InputFile_cue)[inputfileexist]
-d.mvm.cue <- cbind(cue[inputfileexist,], InputFile)
-
-InputFile <- unlist(InputFile_delay)[inputfileexist]
-d.mvm.delay <- cbind(delay[inputfileexist,], InputFile)
-
-InputFile <- unlist(InputFile_resp)[inputfileexist]
-d.mvm.resp <- cbind(resp[inputfileexist,], InputFile)
-
-#rbind all 3 
-d.mvm <- rbind(d.mvm.cue,d.mvm.delay)
-d.mvm <- rbind(d.mvm,d.mvm.resp)
+# repeat dataframe for cue, delay, and resp. set new "condition" column
+d.mvm <-
+  lapply(c("cue","delay","resp"),
+         function(cnd) subjs %>% mutate(condition=cnd)) %>%
+  bind_rows %>%
+  # use 'headfile' to set the location of the named subjbrick and then remove the column
+  mutate(InputFile=sprintf('%s[%s#0_Coef]', headfile, condition)) %>%
+  select(-headfile)
 
 
-
-#### MVM ####
-
-d.mvm<-d.mvm%>%select(-InputFile,InputFile)
+# break up into cue delay and resp files
 d.cue <- subset(d.mvm, condition=="cue")
 d.delay <- subset(d.mvm, condition=="delay")
 d.resp <- subset(d.mvm, condition=="resp")
 
-
-
-#write table
+# save all and individually
 write.table(d.mvm, '/Volumes/Zeus/Orma/7T_MGS/scripts/07212020_mvm_table.txt', quote = FALSE, sep="\t", row.names = FALSE)
 write.table(d.cue, '/Volumes/Zeus/Orma/7T_MGS/scripts/07212020_cue_mvm_table.txt', quote = FALSE, sep="\t", row.names = FALSE)
 write.table(d.delay, '/Volumes/Zeus/Orma/7T_MGS/scripts/07212020_delay_mvm_table.txt', quote = FALSE, sep="\t", row.names = FALSE)
