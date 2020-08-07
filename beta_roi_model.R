14ed30d38992ea59a031861d6ba98ffd5d0dc48f
WillForan <willforan@gmail.com>
2020-08-07 10:43:32 -0400

    🎨 beta_roi_model.R re org

--- beta_roi_model.R
+++ beta_roi_model.R
@@ -8,14 +8,33 @@ library(dplyr)
 library(ggplot2)
 theme_set(cowplot::theme_cowplot())
 
+
+####
+## settings
+####
 z_thres <- 2
 MDL_FML <- list(lin=beta~age+gender,
                 inv=beta~invage+gender,
                 quad=beta~age2+age+gender)
 
+####
+## quick functions for messing with models
+####
+
+# given a (slice of a) dataframe, run lm with each of the named formulas in M (likely, MDL_FML)
+mk_models <- function(d, M) lapply(M, function(f) lm(f,d))
+# given a list of models with names() matching M, pull out the name of best AIC
+best_model <- function(models, M)  names(M)[which.min(sapply(models, AIC))]
+# quick plot
+plotme <- function(d) ggplot(d) + aes(x=age, y=beta) + geom_point(aes(color=gender)) + geom_smooth()
+
+#####
+## read in data
+####
+
 # get data, merge with age, make invage and age2
 d <- read.csv('../group_contrasts/resp_beta_clust_spheres_08042020.txt') %>%
-   left_join(read.table('scan_IDs_ages.txt', header=T)) %>%
+   merge(read.table('scan_IDs_ages.txt', header=T), by="subj", all.x=T) %>%
    mutate(invage=1/age, age2=age**2)
 
 # remove outliers
@@ -25,21 +44,40 @@ clean <- d %>%
    filter(zscore <= z_thres) %>% ungroup
 
 
-# quick functions for messing with models
-mk_models <- function(d, M) lapply(M, function(f) lm(f,d))
-best_model <- function(models, M)  names(M)[which.min(sapply(models, AIC))]
-plotme <- function(d) ggplot(d) + aes(x=age, y=beta) + geom_point(aes(color=gender)) + geom_smooth()
-# apply functions
+
+####
+## Do stuff!
+####
+# - split into dataframes where all rows have the same roi
+# - run mk_models
+# - use MDL_FML as the model formulas
+
 mr_models <- lapply(split(d,d$roi), mk_models, MDL_FML) 
+
+# now we have a list (ea. roi) of lists (ea. formula) containing a bunch of models
+# use 'best_model()' and names of MDL_FML to get which of the models for each roi in MDL_FM (lin,inv,quad) has the best AIC
 mr_best <- sapply(mr_models, best_model, MDL_FML)
+
 # get p vals.
+# use the index/name from mr_best to pull out the summary for the best model.
 # N.B. the models formuals are all written so the coef of interest (age,inv,quad) is always #2 in summary$coef matrix
 pvals <- mapply(function(m,i) summary(m[[i]])$coef[2,'Pr(>|t|)'], mr_models, mr_best)
-# make into a dataframe
-mr_best_fit_and_p <- data.frame(roi=names(pvals), best=mr_best, p=p)
+# see
+#  roi1_models <- mr_models[[1]] # pull out first model (roi 1)
+#  roi1_lin <- summary(roi1_models$lin) # summary of roi1's lin model
+#  print(roi1_lin$coef)
+#  print(roi1_lin$coef[2,'Pr(>|t|)'])
 
+# make into a dataframe
+mr_best_fit_and_p <- data.frame(roi=names(pvals), best=mr_best, p=pvals)
+
+# plot just the best ones
 mr_sig_plt <- mr_best_fit_and_p %>% filter(p<=.01) %>% inner_join(d, by="roi") %>% plotme(.) + facet_wrap(.~roi)
 print(mr_sig_plt)
+
+#####
+## again for MRSI
+####
 
 # TODO: pull in mrsi data
 # mrsi <- read.csv(...)
